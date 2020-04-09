4e3e907fee2abf4fbb5fe40318883cce4d1416cc
Maxim Trofimenko <oklickk@yandex.ru>
2020-04-09 13:43:07 +0300

    добавил конструктор с параметрами в сущность Message

--- src/main/java/com/trofimenko/loom/domain/Message.java
+++ src/main/java/com/trofimenko/loom/domain/Message.java
@@ -18,6 +18,10 @@ public class Message {
 
     public Message() {
     }
+    public Message(String text,String tag){
+        this.text = text;
+        this.tag = tag;
+    }
 
     public Integer getId() {
         return id;
