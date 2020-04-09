7502c2e73e2a68efdc622c9e73f17d63c099b1f6
Maxim Trofimenko <oklickk@yandex.ru>
2020-04-09 13:46:57 +0300

    создал в репозитории метод поиска сообщений по тэгу

--- src/main/java/com/trofimenko/loom/repository/MessageRepository.java
+++ src/main/java/com/trofimenko/loom/repository/MessageRepository.java
@@ -3,5 +3,8 @@ package com.trofimenko.loom.repository;
 import com.trofimenko.loom.domain.Message;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface MessageRepository extends CrudRepository<Message,Long> {
+    List<Message> findByTag(String tag);
 }
