8e5d251403e6eee6fd8abb30039c8379b6440aad
Maxim Trofimenko <oklickk@yandex.ru>
2020-04-10 12:09:10 +0300

    тут вся логика по работе с изменением user через форму и @PostMapping комментарии с пояснениями в коде

--- src/main/java/com/trofimenko/loom/controller/UserController.java
+++ src/main/java/com/trofimenko/loom/controller/UserController.java
@@ -8,6 +8,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 @Controller
 @RequestMapping("/user")
 public class UserController {
@@ -22,10 +28,41 @@ public class UserController {
     }
 
     @GetMapping("{user}")
-    public String userEditForm(@PathVariable User user, Model model){
+    public String userEditForm(
+            @PathVariable User user, //тут мы получаем сразу user по его id
+            Model model
+    ){
         model.addAttribute("user",user);
         model.addAttribute("roles", Role.values());
         return "userEdit";
+    }
+    @PostMapping
+    public String userSave(
+            @RequestParam String username,         //получаем username из формы
+            @RequestParam Map<String,String> form, //получаем список ролей, их может быть разное количество, поэтому Map
+            @RequestParam("userId") User user      //получем сразу user по его id который дергается из form
+    ){
+        user.setUsername(username);
+
+        Set<String> roles = Arrays.stream(Role.values())  //переводим enum в set ролей
+                .map(Role::name)
+                .collect(Collectors.toSet());
+
+        user.getRoles().clear();
+
+        /*
+        добавляем роли пользователю, тоесть сравниваем списов ВСЕХ ролей и ролей которые
+        заполнил пользователь через форму
+        Если роль из формы соответствует роли в базовом списке то добавляем эту роль юзеру
+         */
+        for (String key : form.keySet()) {
+            if (roles.contains(key)) {
+                user.getRoles().add(Role.valueOf(key));
+            }
+        }
+
+        userRepository.save(user);
+        return "redirect:/user";
     }
 
 }
