ec70fed803670a8d03f25058d4a8e9585f2f1349
Maxim Trofimenko <oklickk@yandex.ru>
2020-04-10 09:11:55 +0300

    убрал @PostMapping, теперь запрос на фильтрацию сообщений из формы идет как Get а не Post мы можем из вормы передать запрос как при поможи метода Get так и Post использовать get более правильно в данном случае у нас просто отображается страница main, и параметр filter - required = false, тоесть может и тсутствовать, но если мы захотим применить filter то он передастся в контролле методом get и применится соответствующая фильтрация в uri http://localhost:8080/main?filter=привет

--- src/main/java/com/trofimenko/loom/controller/MainController.java
+++ src/main/java/com/trofimenko/loom/controller/MainController.java
@@ -24,9 +24,17 @@ public class MainController {
     }
 
     @GetMapping("/main")
-    public String main(Model model){
+    public String main(@RequestParam(required = false, defaultValue = "") String filter, Model model){
         Iterable<Message> messages = messageRepository.findAll();
+
+        if (filter != null && !filter.isEmpty()){
+            messages = messageRepository.findByTag(filter);
+        } else
+            messages = messageRepository.findAll();
+
         model.addAttribute("messages", messages);
+        model.addAttribute("filter",filter);
+
         return "main";
     }
 
@@ -41,17 +49,4 @@ public class MainController {
 
         return "main";
     }
-
-    @PostMapping("filter")
-    public String filter(@RequestParam String filter,Model model){
-        Iterable<Message> messages;
-        if (filter != null && !filter.isEmpty()){
-            messages = messageRepository.findByTag(filter);
-        } else
-            messages = messageRepository.findAll();
-
-        model.addAttribute("messages", messages);
-        return "main";
-    }
-
 }
