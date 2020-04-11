05122568b09d0e0f5ea6a8f101e734c5d28f2c7d
Maxim Trofimenko <oklickk@yandex.ru>
2020-04-11 10:46:26 +0300

    перекинул логику по добавлению нового пользователя из контроллера в сервис, так правильнее

--- src/main/java/com/trofimenko/loom/controller/RegistrationController.java
+++ src/main/java/com/trofimenko/loom/controller/RegistrationController.java
@@ -1,22 +1,18 @@
 package com.trofimenko.loom.controller;
 
-import com.trofimenko.loom.domain.Role;
 import com.trofimenko.loom.domain.User;
-import com.trofimenko.loom.repository.UserRepository;
+import com.trofimenko.loom.servise.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import java.util.Collection;
-import java.util.Collections;
-
 @Controller
 public class RegistrationController {
 
     @Autowired
-    private UserRepository userRepository;
+    private UserService userService;
 
     @GetMapping("/registration")
     public String registration(){
@@ -25,18 +21,10 @@ public class RegistrationController {
 
     @PostMapping("/registration")
     public String addUser(User user, Model model){
-        User userFromDB = userRepository.findByUsername(user.getUsername());
-
-        if (userFromDB != null){
+        if (!userService.addUser(user)){
             model.addAttribute("message","User exist!");
             return "registration";
         }
-
-        user.setActive(true);
-        //тут у user создается set с одним единственным значением
-        user.setRoles(Collections.singleton(Role.USER));
-        userRepository.save(user);
-
         return "redirect:/login";
     }
 }
--- src/main/java/com/trofimenko/loom/servise/UserService.java
+++ src/main/java/com/trofimenko/loom/servise/UserService.java
@@ -1,11 +1,15 @@
 package com.trofimenko.loom.servise;
 
+import com.trofimenko.loom.domain.Role;
+import com.trofimenko.loom.domain.User;
 import com.trofimenko.loom.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.Collections;
 
 @Service
 public class UserService implements UserDetailsService {
@@ -16,5 +20,20 @@ public class UserService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return userRepository.findByUsername(username);
+    }
+
+    public boolean addUser(User user){
+        User userFromDB = userRepository.findByUsername(user.getUsername());
+
+        if(userFromDB != null){
+            return false;
+        }
+
+        user.setActive(true);
+        //тут у user создается set с одним единственным значением
+        user.setRoles(Collections.singleton(Role.USER));
+        userRepository.save(user);
+
+        return true;
     }
 }
