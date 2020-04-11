c1346c949b7d1e31df1d599031d1c24c5b1c9f9c
Maxim Trofimenko <oklickk@yandex.ru>
2020-04-11 14:41:32 +0300

    допиливаем контроллер регистрации для активационного кода

--- src/main/java/com/trofimenko/loom/controller/RegistrationController.java
+++ src/main/java/com/trofimenko/loom/controller/RegistrationController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -27,4 +28,19 @@ public class RegistrationController {
         }
         return "redirect:/login";
     }
+
+    @GetMapping("/activate/{code}")
+    public String activate(@PathVariable String code, Model model){
+        boolean isActivated = userService.activeUser(code);
+
+        if(isActivated){
+            model.addAttribute("message", "User successfully activated");
+        }else
+            model.addAttribute("message", "Activation code is note found");
+
+
+        return "login";
+
+    }
+
 }
