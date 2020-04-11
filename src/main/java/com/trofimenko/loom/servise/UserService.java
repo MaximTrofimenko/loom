1d56d7460dca8b337b93de17f8b09a2249a0b7b2
Maxim Trofimenko <oklickk@yandex.ru>
2020-04-11 14:45:26 +0300

    допиливание UserService для поддтверждения почты нового пользователя

--- src/main/java/com/trofimenko/loom/servise/UserService.java
+++ src/main/java/com/trofimenko/loom/servise/UserService.java
@@ -4,18 +4,24 @@ import com.trofimenko.loom.domain.Role;
 import com.trofimenko.loom.domain.User;
 import com.trofimenko.loom.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 import java.util.Collections;
+import java.util.UUID;
 
 @Service
 public class UserService implements UserDetailsService {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private MailSender mailSender;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
@@ -30,8 +36,31 @@ public class UserService implements UserDetailsService {
         }
 
         user.setActive(true);
-        //тут у user создается set с одним единственным значением
-        user.setRoles(Collections.singleton(Role.USER));
+        user.setRoles(Collections.singleton(Role.USER)); //тут у user создается set с одним единственным значением
+        user.setActivationCode(UUID.randomUUID().toString());
+
+        userRepository.save(user);
+
+        if(!StringUtils.isEmpty(user.getEmail())){   //проверка что поле не пустое, если не пусто - отправляем
+            String message = String.format(
+                    "Hello, %s! \n" +
+                            "Welcome to Loom. Please, visit next link: http://localhost:8080/activate/%s",  //url можно вынести в проперти и использовать разные проперти для продакшн и девелоп
+                    user.getUsername(),
+                    user.getActivationCode()
+            );
+
+            mailSender.send(user.getEmail(),"Activation code", message);
+        }
+        return true;
+    }
+
+    public boolean activeUser(String code) {
+        User user = userRepository.findByActivationCode(code);
+        if(user == null){
+            return false;
+        }
+
+        user.setActivationCode(null);
         userRepository.save(user);
 
         return true;
