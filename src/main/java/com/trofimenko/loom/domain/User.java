f8453d0c6a5c0fa68d43bb6e44a14299e0ab9778
MaximTrofimenko <oklickk@yandex.ru>
2020-04-29 09:40:09 +0300

    добавил поля и связи для подписок и подписчиков

--- src/main/java/com/trofimenko/loom/domain/User.java
+++ src/main/java/com/trofimenko/loom/domain/User.java
@@ -9,6 +9,7 @@ import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import java.io.Serializable;
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
@@ -36,6 +37,22 @@ public class User implements UserDetails {
 
     @OneToMany(mappedBy = "author", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private Set<Message> messages;
+
+    @ManyToMany
+    @JoinTable(
+            name = "user_subscriptions",
+            joinColumns = {@JoinColumn(name = "chanel_id")},
+            inverseJoinColumns = {@JoinColumn(name = "subscriber_id")}
+    )
+    private Set<User> subscribers = new HashSet<>();
+
+    @ManyToMany
+    @JoinTable(
+            name = "user_subscriptions",
+            joinColumns = {@JoinColumn(name = "subscriber_id")},
+            inverseJoinColumns = {@JoinColumn(name = "chanel_id")}
+    )
+    private Set<User> subscribtions = new HashSet<>();
 
     @Override
     public boolean equals(Object o) {
@@ -142,5 +159,21 @@ public class User implements UserDetails {
 
     public void setMessages(Set<Message> messages) {
         this.messages = messages;
+    }
+
+    public Set<User> getSubscribers() {
+        return subscribers;
+    }
+
+    public void setSubscribers(Set<User> subscribers) {
+        this.subscribers = subscribers;
+    }
+
+    public Set<User> getSubscribtions() {
+        return subscribtions;
+    }
+
+    public void setSubscribtions(Set<User> subscribtions) {
+        this.subscribtions = subscribtions;
     }
 }
